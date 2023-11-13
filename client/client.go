40855663a01a5279667c8fa194f00f6377e898db
bruce-mig <h180009t@hit.ac.zw>
2023-11-13 11:34:15 +0200

    added client func calls to grpc server

--- client/client.go
+++ client/client.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	pb "github.com/bruce-mig/pricefetcher-grpc-microservices/proto"
 	"github.com/bruce-mig/pricefetcher-grpc-microservices/types"
@@ -12,16 +16,91 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+var ctx, _ = context.WithTimeout(context.Background(), 3000*time.Second)
+
 func NewGRPCClient(remoteAddr string) (pb.PriceFetcherClient, error) {
 	conn, err := grpc.Dial(remoteAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
+
+	defer conn.Close()
+
 	c := pb.NewPriceFetcherClient(conn)
 
 	return c, nil
 }
 
+// gRPC Client
+func CallFetchPrice(wg *sync.WaitGroup, grpcClient pb.PriceFetcherClient, symbols *pb.SymbolsList) {
+	// defer wg.Done()
+	time.Sleep(3 * time.Second)
+	resp, err := grpcClient.FetchPrice(ctx, symbols.Symbols[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", resp)
+}
+
+func CallFetchPriceServerStreaming(wg *sync.WaitGroup, grpcClient pb.PriceFetcherClient, symbols *pb.SymbolsList) {
+	// defer wg.Done()
+	log.Print("Streaming started")
+
+	stream, err := grpcClient.FetchPriceServerStreaming(ctx, symbols)
+	if err != nil {
+		log.Fatalf("Could not send symbols: %v", err)
+	}
+
+	for {
+		message, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while streaming %v", err)
+		}
+		log.Println(message)
+	}
+	log.Printf("Streaming finished")
+}
+
+func CallFetchPriceBidirectionalStreaming(wg *sync.WaitGroup, grpcClient pb.PriceFetcherClient, symbols *pb.SymbolsList) {
+	defer wg.Done()
+	log.Printf("Bidirectional Streaming Started")
+	stream, err := grpcClient.FetchPriceBidirectionalStreaming(ctx)
+	if err != nil {
+		log.Fatalf("Could not send symbols: %v", err)
+	}
+	waitCh := make(chan struct{})
+	go func() {
+		for {
+			message, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while streaming %v", err)
+			}
+			log.Println(message)
+		}
+		close(waitCh)
+	}()
+
+	for _, symbol := range symbols.Symbols {
+		req := &pb.PriceRequest{
+			Symbol: symbol.Symbol,
+		}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("Error while sending %v", err)
+		}
+		time.Sleep(2 * time.Second)
+	}
+	stream.CloseSend()
+	<-waitCh
+	log.Printf("Bidirectional streaming finished")
+}
+
+// JSON client
 type Client struct {
 	endpoint string
 }
