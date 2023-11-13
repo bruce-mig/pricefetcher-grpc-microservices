71615edb4f91d3d06c9112fad2aaa7febca8fdd3
bruce-mig <h180009t@hit.ac.zw>
2023-11-13 11:30:53 +0200

    added bidirectional stream server

--- grpc_server.go
+++ grpc_server.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 
 	pb "github.com/bruce-mig/pricefetcher-grpc-microservices/proto"
@@ -19,9 +21,8 @@ type ctxKey string
 // Define a constant for the request ID key
 const requestIDKey ctxKey = "requestID"
 
-var reqID int = rand.Intn(100000)
-
-func makeGRPCServerAndRun(listenAddr string, svc PriceService) error {
+func makeGRPCServerAndRun(wg *sync.WaitGroup, listenAddr string, svc PriceService) error {
+	defer wg.Done()
 	grpcPriceFetcher := NewGRPCPriceFetcherServer(svc)
 
 	ln, err := net.Listen("tcp", listenAddr)
@@ -50,7 +51,9 @@ func NewGRPCPriceFetcherServer(svc PriceService) *GRPCPriceFetcherServer {
 func (s *GRPCPriceFetcherServer) FetchPrice(ctx context.Context, req *pb.PriceRequest) (*pb.PriceResponse, error) {
 
 	// Set the request ID in the context using the custom key type
+	reqID := rand.Intn(10000)
 	ctx = context.WithValue(ctx, requestIDKey, reqID)
+
 	body, err := s.svc.FetchPrice(ctx, req.Symbol)
 	if err != nil {
 		return nil, err
@@ -79,6 +82,10 @@ func (s *GRPCPriceFetcherServer) FetchPriceServerStreaming(req *pb.SymbolsList, 
 	log.Printf("Got request with symbols: %v", req.Symbols)
 	ctx := context.Background()
 	for _, symbol := range req.Symbols {
+		//req ID
+		reqID := rand.Intn(10000)
+		ctx = context.WithValue(ctx, requestIDKey, reqID)
+
 		resp, err := s.FetchPrice(ctx, symbol)
 		if err != nil {
 			return err
@@ -89,4 +96,30 @@ func (s *GRPCPriceFetcherServer) FetchPriceServerStreaming(req *pb.SymbolsList, 
 		time.Sleep(2 * time.Second)
 	}
 	return nil
+}
+
+func (s *GRPCPriceFetcherServer) FetchPriceBidirectionalStreaming(stream pb.PriceFetcher_FetchPriceBidirectionalStreamingServer) error {
+	ctx := context.Background()
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("Got request with symbol:%v", req.Symbol)
+
+		//req ID
+		reqID := rand.Intn(10000)
+		ctx = context.WithValue(ctx, requestIDKey, reqID)
+
+		resp, err := s.FetchPrice(ctx, req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(resp); err != nil {
+			return err
+		}
+	}
 }
